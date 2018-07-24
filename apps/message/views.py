a8e0d2e77e0b405ae8a7bcf133770e094ab95a91
smh2028 <smh2208@163.com>
2018-07-24 18:16:39 +0800

    前端修改

--- apps/message/views.py
+++ apps/message/views.py
@@ -10,6 +10,6 @@ def getform(request,id):
     #UserMessage默认的数据管理器是objects
     #方法all()将所有数据返回成一个queryset类型
     all_message = UserMessage.objects.all()
-    for message in all_message:
-        print(message.name)
+    # for message in all_message:
+    #     print(message.name)
     return render(request,'message_form.html',{'my_message':all_message[id]})
