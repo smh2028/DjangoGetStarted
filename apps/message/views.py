4c2e31028b25ed607f25b793cee342782267aac4
smh2028 <smh2208@163.com>
2018-07-25 14:20:46 +0800

    url参数可变，支持大小写

--- apps/message/views.py
+++ apps/message/views.py
@@ -11,10 +11,16 @@ def getform(request):
     #方法all()将所有数据返回成一个queryset类型
 
     w = request.GET.get('w')
-    t = request.GET.get('t')
-    ms = request.GET.get('ms')
-    g = request.GET.get('g')
-    np = request.GET.get('np')
-    id = 'W' + w + 'T' + t + 'MS' + ms + 'G' + g + 'NP' + np
-    message = UserMessage.objects.get(serial_num=id)
+    params = request.GET.dict()
+    #拼接关键字参数,获得serail_num
+    sn = ''
+    for k,v in params.items():
+        sn += k.upper()+v
+    # print('sn',sn)
+    # t = request.GET.get('t')
+    # ms = request.GET.get('ms')
+    # g = request.GET.get('g')
+    # np = request.GET.get('np')
+    # id = 'W' + w + 'T' + t + 'MS' + ms + 'G' + g + 'NP' + np
+    message = UserMessage.objects.get(serial_num=sn)
     return render(request,'message_form.html',{'my_message':message})
