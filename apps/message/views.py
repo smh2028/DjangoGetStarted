28200d9432c52426a5fb99002ac98230a7751a55
smh2028 <smh2208@163.com>
2018-07-25 15:01:07 +0800

    query params test

--- apps/message/views.py
+++ apps/message/views.py
@@ -10,17 +10,12 @@ def getform(request):
     #UserMessage默认的数据管理器是objects
     #方法all()将所有数据返回成一个queryset类型
 
-    w = request.GET.get('w')
     params = request.GET.dict()
     #拼接关键字参数,获得serail_num
     sn = ''
     for k,v in params.items():
         sn += k.upper()+v
-    # print('sn',sn)
-    # t = request.GET.get('t')
-    # ms = request.GET.get('ms')
-    # g = request.GET.get('g')
-    # np = request.GET.get('np')
-    # id = 'W' + w + 'T' + t + 'MS' + ms + 'G' + g + 'NP' + np
+    print('sn',sn,type(sn))
+    print('items:',params.items())
     message = UserMessage.objects.get(serial_num=sn)
-    return render(request,'message_form.html',{'my_message':message})
+    return render(request,'message_form.html',{'my_message': message})
