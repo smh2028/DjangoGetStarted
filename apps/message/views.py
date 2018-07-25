3465a5341c475e8dc4d38e8865a599af916530b5
smh2028 <smh2208@163.com>
2018-07-25 15:31:03 +0800

    query params re-design

--- apps/message/views.py
+++ apps/message/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 
 from .models import UserMessage
+import re
 # Create your views here.
 
 def getform(request):
@@ -10,12 +11,12 @@ def getform(request):
     #UserMessage默认的数据管理器是objects
     #方法all()将所有数据返回成一个queryset类型
 
-    params = request.GET.dict()
-    #拼接关键字参数,获得serail_num
+    full_path = request.get_full_path()
+    r = re.search('\?(\w)=(\d{4})&(\w)=(\d{4})&(\w{2})=(\d)&(\w)=(\d)&(\w{2})=(\d{7})',full_path).groups()
     sn = ''
-    for k,v in params.items():
-        sn += k.upper()+v
-    print('sn',sn,type(sn))
-    print('items:',params.items())
+    for i in r:
+        sn += i
+    sn = sn.upper()
+    print(sn)
     message = UserMessage.objects.get(serial_num=sn)
     return render(request,'message_form.html',{'my_message': message})
